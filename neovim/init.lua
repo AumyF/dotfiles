87506d64cc57d8ff65b008757f6649f4c9659290
Aumy <aumy@outlook.jp>
2022-12-16 23:31:39 +0900

    Nvim: add lualine

--- neovim/init.lua
+++ neovim/init.lua
@@ -56,6 +56,14 @@ require('packer').startup(function(use)
 
   use 'akinsho/toggleterm.nvim'
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require'lualine'.setup()
+    end
+  }
+
   use 'ionide/Ionide-vim'
  
   use 'lukas-reineke/indent-blankline.nvim'
