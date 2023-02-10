19362bad6affeec2e312f82ef776a91be12ccda7
Aumy <aumy@outlook.jp>
2023-02-10 23:06:56 +0900

    Nvim: set keymap for lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -225,8 +225,12 @@ require 'lspconfig'.sumneko_lua.setup {
       diagnostics = { globals = { 'vim' } }
     },
     workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-    telemetry = { enable = false }
-  }
+    telemetry = { enable = false },
+  },
+  on_attach = function(client, bufnr)
+    lsp_set_keymap(client, bufnr)
+  end
+
 }
 
 
