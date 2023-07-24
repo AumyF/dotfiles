f651754a08f5b02873c81ddcde3c4f135b7beab6
Aumy <aumy@outlook.jp>
2023-07-24 16:38:14 +0900

    Nvim: fix telescope

--- neovim/init.lua
+++ neovim/init.lua
@@ -207,7 +207,6 @@ require("lazy").setup({
 				},
 			})
 			require("telescope").load_extension("undo")
-			require("telescope").load_extension("file_browser")
 			require("telescope").load_extension("fzf")
 		end,
 	},
