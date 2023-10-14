ea920be05fc70e305e0453d239aa720b69171a95
Aumy <aumy@outlook.jp>
2023-10-15 02:27:45 +0900

    Tmp

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'Sonokai (Gogh)'
-config.font = wezterm.font 'JetBrainsMono'
+config.force_reverse_video_cursor = true
+config.font = wezterm.font 'UDEV Gothic 35NF'
+config.line_height = 1.2
 config.font_size = 10
 
 return config
